3c19d8e077577026fd89f3fc2e3d02bf6724f51c
Caio Nunes <git@caionunes.dev>
2024-06-18 04:22:57 -0300

    Admin panel

--- cndev-custom-css.php
+++ cndev-custom-css.php
@@ -55,6 +55,15 @@ if ( ! defined( 'CNDCCSS__FILES' ) ) {
 	);
 }
 
+if ( ! defined( 'CNDCCSS__MIN' ) ) {
+	/**
+	 * Whether to search for minified version of files.
+	 * 
+	 * @since 1.0.0
+	 */
+	define( 'CNDCCSS__MIN', false );
+}
+
 if ( ! defined( 'CNDCCSS__URL' ) ) {
 	/**
 	 * Custom CSS plugin root directory.
@@ -145,3 +154,100 @@ if ( ! function_exists( 'cndev__enqueues' ) ) {
 	add_action( 'wp_enqueue_scripts', 'cndev__enqueues', CNDCCSS__HOOK_PRIORITY );
 	add_action( 'admin_enqueue_scripts', 'cndev__enqueues', CNDCCSS__HOOK_PRIORITY );
 }
+
+if ( ! function_exists( 'cndev__styles_post_type' ) ) {
+	/**
+	 * Generates the stylesheet post-type.
+	 */
+	function cndev__styles_post_type() {
+		$args   = array(
+			'description'        => 'Portfolio stylesheets.',
+			'public'             => false,
+			'publicly_queryable' => false,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'menu_icon'          => 'dashicons-admin-customizer',
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'stylesheet' ),
+			'capability_type'    => 'post',
+			'has_archive'        => false,
+			'hierarchical'       => false,
+			'supports'           => array( 'title' ),
+			'show_in_rest'       => false,
+			'labels'             => array(
+				'name'                  => __( 'Stylesheets', 'cndev' ),
+				'singular_name'         => __( 'Stylesheet', 'cndev' ),
+				'menu_name'             => __( 'Custom CSS', 'cndev' ),
+				'name_admin_bar'        => __( 'Custom CSS', 'cndev' ),
+				'add_new'               => __( 'Add New', 'cndev' ),
+				'add_new_item'          => __( 'Add New Stylesheet', 'cndev' ),
+				'new_item'              => __( 'New stylesheet', 'cndev' ),
+				'edit_item'             => __( 'Edit stylesheet', 'cndev' ),
+				'view_item'             => __( 'View stylesheet', 'cndev' ),
+				'all_items'             => __( 'All stylesheets', 'cndev' ),
+				'search_items'          => __( 'Search stylesheets', 'cndev' ),
+				'parent_item_colon'     => __( 'Parent stylesheets:', 'cndev' ),
+				'not_found'             => __( 'No stylesheet found.', 'cndev' ),
+				'not_found_in_trash'    => __( 'No stylesheet found in Trash.', 'cndev' ),
+				'featured_image'        => __( 'Stylesheet thumbnail', 'cndev' ),
+				'set_featured_image'    => __( 'Set image', 'cndev' ),
+				'remove_featured_image' => __( 'Remove image', 'cndev' ),
+				'use_featured_image'    => __( 'Use as image', 'cndev' ),
+				'archives'              => __( 'Stylesheet archives', 'cndev' ),
+				'insert_into_item'      => __( 'Insert into stylesheet', 'cndev' ),
+				'uploaded_to_this_item' => __( 'Uploaded to this stylesheet', 'cndev' ),
+				'filter_items_list'     => __( 'Filter stylesheets list', 'cndev' ),
+				'items_list_navigation' => __( 'Stylesheets list navigation', 'cndev' ),
+				'items_list'            => __( 'stylesheets list', 'cndev' ),
+			),
+		);
+		register_post_type( 'cndev__stylesheets', $args );
+	}
+	add_action( 'init', 'cndev__styles_post_type' );
+}
+
+/**
+ * Add meta-boxes
+ * 
+ * Reference (name/title);
+ * Path (upload file);
+ * 
+ */
+
+function cndev__create_meta_box() {
+
+}
+
+
+
+
+/*
+if ( ! function_exists( 'cndev__get_styles' ) ) {
+	function cndev__get_styles() {
+		$styles             = array_diff( scandir( CNDCCSS__PATH . 'styles' ), array( '..', '.' ) );
+		$styles_to_enqueue  = array();
+
+		// echo '<div style="background: #fff; width: 100dvw; min-height: 100dvh; position: absolute; left: 0; top: 0; z-index: 999999999999999999999999"><pre>';
+
+		foreach ( $styles as $style ) {
+			if ( str_ends_with( $style, '.css' ) ) {
+				$style_path = cndev__get_path( $styles, $style );
+				if ( ! in_array( $style_path, $styles_to_enqueue, true ) ) {
+					$styles_to_enqueue[] = $style_path;
+				}
+			}
+		}
+		return $styles_to_enqueue;
+		// echo '</pre></div>';
+	}
+}
+
+if ( ! function_exists( 'cndev__get_path' ) ) {
+	function cndev__get_path( array $styles, string $style ) {
+		$style = str_replace( '.min', '', $style );
+		$style_name = explode( '.', $style, 2 )[0];
+	
+		return CNDCCSS__URL . 'styles/' . CNDCCSS__MIN && in_array( $style_name . '.min.css', $styles, true ) ?  $style_name . '.min.css' : $style;
+	}
+}
+*/
